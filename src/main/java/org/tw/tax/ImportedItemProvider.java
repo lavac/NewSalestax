870fd33577981293233693d7668e3f6e5daab402
lavac <lavanyacs743@gmail.com>
2017-06-19 12:29:02 +0530

    Implement salestax

--- src/main/java/org/tw/tax/ImportedItemProvider.java
+++ src/main/java/org/tw/tax/ImportedItemProvider.java
@@ -1,13 +1,6 @@
 package org.tw.tax;
 
-/**
- * Created by lavanya on 6/13/17.
- */
-
-
 public class ImportedItemPrivider implements ItemProvider {
-
-
     private static final String IMPORTED_FACTOR = " imported ";
 
     @Override
